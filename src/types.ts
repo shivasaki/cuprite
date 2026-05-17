ac5f4ad3b73508462314e520e5d17914328e4ab2
hoshi <shivasaki0329@gmail.com>
2026-05-17 16:01:58 +0900

    Initial commit

---
+++ src/types.ts
@@ -0,0 +1,10 @@
+export interface MyWork {
+    title: string;
+    overview?: string;
+    description?: string;
+    linkTitle?: string;
+    url?: string;
+    image?: string;
+    year?: string;
+  }
+  
